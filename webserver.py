549133b357a5ef35529b0ab3c504a1e6c34104f7
SABARI005 <9003602285sabs@gmail.com>
2022-12-12 15:16:44 +0000

    Aim Changes

--- webserver.py
+++ webserver.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-Develop a webserver to display about top five web application development frameworks.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content="""
@@ -49,14 +31,3 @@ print("This is my Webserver")
 server_address=('',8000)
 httpd=HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-```
-
-## OUTPUT:
-### Client side:
-![alt text](./images/client.png)
-### Server side:
-![alt text](./images/Untitled.png)
-
-
-## RESULT:
-The program is executed succesfully.
